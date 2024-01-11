3eb9d38aeff6c77defa48bdc23801a7d116a1697
Lucas Baptista <Lucas.jesus.13>
2024-01-11 09:25:44 -0300

    atualização

--- dado/menu.js
+++ dado/menu.js
@@ -1,9 +1,9 @@
 export default class Menu {
   static listar() {
     var lista = []
-    lista.push({ codigo: 1, nivel: 999,importante:1, descricao: "Competência", pagina: "" })
-    lista.push({ codigo: 2, nivel: 999,importante:1, descricao: "Entrada", pagina: "entrada" })
-    lista.push({ codigo: 2, nivel: 999,importante:1, descricao: "Saída", pagina: "saida" })
+    lista.push({ codigo: 1, nivel: 999,importante:0, descricao: "Competência", pagina: "" })
+    lista.push({ codigo: 2, nivel: 999,importante:0, descricao: "Entrada", pagina: "entrada" })
+    lista.push({ codigo: 2, nivel: 999,importante:0, descricao: "Saída", pagina: "saida" })
     return lista
 
   }
