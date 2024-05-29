c966ef181d2a38198408d7746e3b43ac7b429633
Lucas Baptista <Lucas.jesus.13>
2024-05-29 11:49:18 -0300

    gddfg

--- pages/entrada.js
+++ pages/entrada.js
@@ -69,6 +69,9 @@ function Entrada() {
                             Descrição
                         </th>
                         <th>
+                            Período
+                        </th>
+                        <th>
                             Valor
                         </th>
                         <th>
